959e311baec1dbb47f7efaa006c20ede30682784
Bayezid <hiperx1@gmail.com>
2025-05-14 03:59:52 +0600

    Add social links to ComingSoonPage and implement useMediaQuery hook for responsive design

---
+++ src/app/(root)/subscribe/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoonPage from "@/components/coming-soon/coming-soon";
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <div>
+      <ComingSoonPage />
+    </div>
+  );
+};
+
+export default Subscribe;
---
+++ src/app/(root)/tools/page.tsx
@@ -0,0 +1,8 @@
+import ComingSoonPage from "@/components/coming-soon/coming-soon";
+import React from "react";
+
+const Tools = () => {
+  return <ComingSoonPage />;
+};
+
+export default Tools;
